b87f672713931410f823933c22d953d0da96c57f
kplachaaa <platacskristaps@gmail.com>
2022-09-06 10:15:50 +0200

    authentication finishing touches

--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,37 +1,12 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
-import { 
-    auth,
-    signInWithGooglePopup, 
-    createUserDocumentFromAuth, 
-    signInWithGoogleRedirect,
-} from "../../utils/firebase/firebase.utils"
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
+import "./authentication.syles.scss";
+
+
 const Authentication = () => {
-    useEffect(async () => {
-        const response = await getRedirectResult(auth);
-        
-        if(response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-        }
-    },[])
-
-
-
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
-
     return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}> Sign in with google </button>
-            <button onClick={signInWithGoogleRedirect}> Sign in with google redirect </button>
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm /> 
         </div>
