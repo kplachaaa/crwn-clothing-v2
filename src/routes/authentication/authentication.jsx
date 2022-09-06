56b67b5c411d52f4e5db94e8d650c3f1be1ec62d
kplachaaa <platacskristaps@gmail.com>
2022-09-06 10:07:19 +0200

    making hcanges

--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -9,9 +9,9 @@ import {
 } from "../../utils/firebase/firebase.utils"
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-
-const SignIn = () => {
+const Authentication = () => {
     useEffect(async () => {
         const response = await getRedirectResult(auth);
         
@@ -32,9 +32,10 @@ const SignIn = () => {
             <h1>Sign In</h1>
             <button onClick={logGoogleUser}> Sign in with google </button>
             <button onClick={signInWithGoogleRedirect}> Sign in with google redirect </button>
+            <SignInForm />
             <SignUpForm /> 
         </div>
     )
 }
 
-export default SignIn;
+export default Authentication;
